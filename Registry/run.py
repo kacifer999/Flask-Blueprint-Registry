67bdf5c7d490606a20be99f660fd7e1bfe459d33
kacifer999 <kacifer999@hotmail.com>
2020-06-01 17:56:44 +0800

    Delete run.py

--- Registry/run.py
+++
@@ -1,7 +0,0 @@
-from api import serve_app
-from flask import Flask
-from gevent.pywsgi import WSGIServer
-print('Api Start Loading.')
-app = serve_app()
-print('Api Start Serving.')
-WSGIServer(('0.0.0.0', 5000), application=app).serve_forever()
